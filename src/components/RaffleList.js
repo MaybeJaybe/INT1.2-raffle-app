d947edc173439223a3a4318e386d6b562ac40476
MaybeJaybe <69604238+MaybeJaybe@users.noreply.github.com>
2021-01-12 15:01:29 -0800

    made onclick remove previous input

--- src/components/RaffleList.js
+++ src/components/RaffleList.js
@@ -6,10 +6,18 @@ function random(n) {
 }
 class RaffleList extends React.Component {
 
-    state = { nameList: [], selectedName: null };
+    constructor(props) {
+        super(props)
+        this.state = { nameList: [], selectedName: null, name: ''}
+        this.addName = this.addName.bind(this)
+        this.removeName = this.removeName.bind(this)
+    }
 
     addName() {
-      this.setState({ nameList: [...this.state.nameList, this.state.name]});
+        if(this.state.name !== '') {
+            this.setState({ nameList: [...this.state.nameList, this.state.name], name: ''});
+        }
+
     }
     removeName(index) {
         this.state.nameList.splice(index, 1)
@@ -18,22 +26,25 @@ class RaffleList extends React.Component {
 
     render() {
         return (
-            <div>
-                <input className='AddName' type='text' value={this.state.name} onChange={(e) => {
-                    this.setState({name: e.target.value})
-                }}/>
-                <button className='AddButton' onClick={() => this.addName()}>
-                    &#x2b;
-                </button>
-                {this.state.nameList.map((name, i) => (<div>{name}
-                    <button className='RemoveButton' onClick={() => this.removeName(i)}>-</button>
-                </div>))}
+            <div className='Form'>
+                <div className='AddForm'>
+                    <input className='AddName' type='text' value={this.state.name} onChange={(e) => {
+                        this.setState({name: e.target.value})
+                        }}/>
+                        <button className='AddButton' onClick={() => this.addName()}>&#x2b;</button>
+                </div>
+
                 <button className='RaffleButton' onClick={() => {
                     this.setState({selectedName: random(this.state.nameList.length)})
                 }}>Raffle</button>
+                
+                {this.state.nameList.map((name, i) => (<div className='List'><p className='NameList' >{name}</p>
+                    <button className='RemoveButton' onClick={() => this.removeName(i)}>&#x2212;</button>
+                </div>))}
+
                 <div className='Result' >
                     Winner:
-            {this.state.selectedName !== null && <h2>{this.state.nameList[this.state.selectedName]}</h2>}
+            {this.state.selectedName !== null && <h2 className='DisplayWinner'>{this.state.nameList[this.state.selectedName]}</h2>}
                 </div>
           </div>
         )
